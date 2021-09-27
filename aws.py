961428c683021291f1e214d164c827568e9c0852
kumar utkarsh <kumarutkarsh462@gmail.com>
2021-09-27 16:04:28 +0530

    md

--- aws.py
+++ aws.py
@@ -1,28 +1,3 @@
-##from googleapiclient import discovery
-##from googleapiclient import errors
-
-
-##project_id = 'projects/{}'.format('YOUR_PROJECT_ID')
-
-
-##ml = discovery.build('ml', 'v1')
-
-
-##request_dict = {'name': 'your_model_name', 'description': 'your_model_description'}
-
-
-##request = ml.projects().models().create(parent=project_id, body=request_dict)
-
-
-##try:
-  ##  response = request.execute()
-    ##print(response)
-##except errors.HttpError, err :
-  ##  print('There was an error creating the model. Check the details:')
-    ##print(err._get_reason())
-
-
-
 num=int(input("Enter a number:"))
 temp=num
 rev=0
